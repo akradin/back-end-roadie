f405629226dadfc4f0d1f458b640a882b9616c60
akradin <alex.kradin@gmail.com>
2017-01-18 14:46:24 -0500

    Began working on validation
    Going to deploy before continuing.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < ActiveRecord::Base
   belongs_to :band
   # validates :name, presence: true
-  # validates :phone_number, numericality: { only_integer: true }
-  # validates :phone_number, length: { is: 10 }
+  # validates :phone_number, presence: false,
+  #                          numericality: { only_integer: true },
+  #                          length: { is: 10 }
 end
